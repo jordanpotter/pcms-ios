3d25bc530cbd71b08b119f73447d94491e2eae9d
Jordan Potter <me@jordanpotter.com>
2014-07-22 14:47:19 -0700

    Initial Commit

---
+++ pcmsTests/pcmsTests.swift
@@ -0,0 +1,36 @@
+//
+//  pcmsTests.swift
+//  pcmsTests
+//
+//  Created by Jordan Potter on 7/22/14.
+//  Copyright (c) 2014 Jordan Potter. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class pcmsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
