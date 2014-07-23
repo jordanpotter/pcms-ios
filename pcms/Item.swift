411d6e18a086e488fe485e41ce58a0d3c3850db6
Jordan Potter <me@jordanpotter.com>
2014-07-23 15:52:43 -0700

    Fixes scanned items table view controller in storyboard
    Begins adding functionality to items

--- pcms/Item.swift
+++ pcms/Item.swift
@@ -10,4 +10,29 @@ import Foundation
 
 struct Item {
 	var id: String
+	var shelf: String?
+	var width: Float?
+	var height: Float?
+	var area: Float? {
+		get {
+			if !self.width || !self.height {
+				return nil
+			} else {
+				return self.width! * self.height!
+			}
+		}
+	}
+	
+	init(id: String) {
+		self.id = id
+	}
+	
+	mutating func saturateData(completionHandler: (NSError?) -> ()) {
+		NSLog("TODO: load item data here")
+		self.width = 17.0
+		self.height = 13.9
+		self.shelf = "shelf 1"
+		
+		completionHandler(nil)
+	}
 }
